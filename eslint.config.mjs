3249adf0c5319dec0c1315c53348d077c69ec2ff
Luke Sullivan <luke@sukelully.dev>
2025-06-06 16:00:50 +0100

    set up prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,18 @@
 import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended", "prettier"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {},
+    rules: {
+      ...prettierConfig.rules,
+    },
+    extends: [js.configs.recommended],
   },
 ]);
