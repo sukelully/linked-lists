4e94632595ca760b1da1c29f6732107b2f155f2f
Luke Sullivan <luke@sukelully.dev>
2025-06-08 10:34:51 +0100

    implement tail logic for append

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -2,32 +2,19 @@ import Node from "./Node.js";
 
 export default function LinkedList() {
   let head = null;
-  // let tail = null;
-  
+  let tail = null;
+
   const append = (val) => {
     const node = Node(val);
-    let current = head;
-    
-  //   if (!head) {
-  //     console.log(`head: ${node.getVal()}`);
-  //     tail = node;
-  //     head = node;
-  //   } else {
-  //     tail.setNext(node);
-  //     tail = tail.getNext();
-  //     current.setNext(node);
-  //   }
-  // };
 
-  if (!head) {
-    head = node;
-  } else {
-    while (current.getNext()) {
-      current = current.getNext();
+    if (!head) {
+      tail = node;
+      head = node;
+    } else {
+      tail.setNext(node);
+      tail = node;
     }
-    current.setNext(node);
-  }
-};
+  };
 
   const toString = () => {
     let outputString = `( ${head.getVal()} ) -> `;
